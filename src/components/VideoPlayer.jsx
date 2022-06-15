fbf073fd663759dc80960e93cb1fd1b4bfc4dd3b
David Arredondo <dcarredondo90@gmail.com>
2022-06-14 18:14:39 -0700

    Completed video player

--- src/components/VideoPlayer.jsx
+++ src/components/VideoPlayer.jsx
@@ -2,17 +2,14 @@
 // so when videoPlayer gets called, it will have the correct
 // object passed in as an argument
 
-// once we figure out what to pass in...
-// I think we also need to import this file into the App.jsx file
 var VideoPlayer = (props) => (
-  {console.log(props)}
   <div className="video-player">
     <div className="embed-responsive embed-responsive-16by9">
-      <iframe className="embed-responsive-item" src="https://www.youtube.com/embed/dQw4w9WgXcQ?autoplay=1" allowFullScreen></iframe>
+      <iframe className="embed-responsive-item" src={'https://www.youtube.com/embed/' + props.video.id.videoId} allowFullScreen></iframe>
     </div>
     <div className="video-player-details">
-      <h3>Video Title</h3>
-      <div>Video Description</div>
+      <h3>{props.video.snippet.title}</h3>
+      <div>{props.video.snippet.description}</div>
     </div>
   </div>
 );
